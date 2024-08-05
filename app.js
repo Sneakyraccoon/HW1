ad49457f644c226b0b16cc5faa204138609a18e9
Sneakyraccoon <zhuriko@gmail.com>
2024-08-05 13:46:10 +0300

    Update app.js

--- app.js
+++ app.js
@@ -8,7 +8,7 @@
 //const MaxValue = 100;
 
 // Попросимо користувача ввести число від 0 до 100
-let MaxValue = prompt("Please enter a number from 0 to 100", 0);
+let MaxValue = prompt("Please enter an integer from 0 to 100", 0);
 
 // Перетворимо значення, яке ввів користувач, щоб точно отримати намбер
 MaxValue = Number.parseInt(MaxValue, 10);
@@ -18,12 +18,28 @@ if (isNaN(MaxValue)) {
     alert("Введене значення не є числом");
   }
 
-//console.log(MaxValue);
+  console.log(MaxValue);
+
+  // Ця ф-ція поверне "true" якщо число (num) є кратним дільнику (divisor)
+  function isMultiple(num, divisor) {
+    return num % divisor === 0;
+  }
+
+
 
 for (let index = 0; index <= MaxValue; index++) {
-    if (index % 3) {
-        else if (index % 5)
+ //   if (index % 3) { if (index % 5){} }
+        
+ if ( isMultiple(index, 3) === true ) { 
+    if (isMultiple(index, 5) === true) { 
+        console.log( `ЛолКек`); // Число кратне і 3 і 5
+    } else {
+        console.log( `Лол`); // Число кратне тільки 3 
     }
-     
+
     
+ } else if (isMultiple(index, 5) === true) {
+    console.log( `Кек`); // Число кратне тільки 5
+ } else
+
 }
