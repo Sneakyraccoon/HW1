12653e51fbaaee56b1081b32b0e8c2a67189ae95
Sneakyraccoon <zhuriko@gmail.com>
2024-08-18 17:07:45 +0300

    Update app.js

--- app.js
+++ app.js
@@ -63,7 +63,7 @@ function durationBetweenDates(
   }
 }
 
-console.log(durationBetweenDates());
+console.log(durationBetweenDates()); // Із параментрами за замовчуванням
 console.log(durationBetweenDates("02 Aug 1985", "03 Aug 1985", "seconds")); // поверне '86400 seconds'
 console.log(durationBetweenDates("31 Jan 2022", "03 Feb 2021", "days")); // поверне '362 days'
 
